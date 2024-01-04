60e3438ef280364e906f7f7f80e62302bb6f9428
shubhansu-kr <shubhansu2021@gmail.com>
2024-01-04 23:11:34 +0530

    Find the min operation req

---
+++ 01_January_2024/03_MinOperation.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/minimum-number-of-operations-to-make-array-empty/?envType=daily-question&envId=2024-01-04
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int res = 0;
+        int s = 0;
+        while(s < nums.size()){
+            int e = s;
+            
+            while(e < nums.size() && nums[e] == nums[s]) {
+                e++;
+            }
+            int count = e-s;
+            if(count == 1) return -1;
+            res += count/3;
+
+            if(count%3 != 0) res++;
+            s=e;
+        }
+        return res;
+       
+    }
+};
+
+int main () {
+    
+    return 0;
+}
