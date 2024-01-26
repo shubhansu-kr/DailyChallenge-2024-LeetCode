1c9671164adbb181c5b5c8e5f00668449aab9176
shubhansu-kr <shubhansu2021@gmail.com>
2024-01-26 22:38:36 +0530

    Find the out of boundry

---
+++ 01_January_2024/25_OutOfBoundry.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/out-of-boundary-paths/?envType=daily-question&envId=2024-01-26
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int m, n;
+    int memo[50][50][51];
+    int DIR[5] = {0, 1, 0, -1, 0};
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        this->m = m; this->n = n;
+        memset(memo, -1, sizeof(memo));
+        return dp(startRow, startColumn, maxMove);
+    }
+    int dp(int r, int c, int maxMove) {
+        if (r < 0 || r == m || c < 0 || c == n) return 1; // Out of bound -> Count 1 way
+        if (maxMove == 0) return 0;
+        if (memo[r][c][maxMove] != -1) return memo[r][c][maxMove];
+        int ans = 0;
+        for (int i = 0; i < 4; ++i)
+            ans = (ans + dp(r + DIR[i], c + DIR[i+1], maxMove - 1)) % 1000000007;
+        return memo[r][c][maxMove] = ans;
+    }
+};
+
+int main () {
+    
+    return 0;
+}
