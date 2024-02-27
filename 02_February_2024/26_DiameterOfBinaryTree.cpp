1e1593b4478732e156193eb9b215f12b0b3aebe7
Shubhansu kumar <shubhansu2021@gmail.com>
2024-02-27 22:14:42 +0530

    Find the diameter of binary tree

--- 02_February_2024/26_DiameterOfBinaryTree.cpp
+++ 02_February_2024/26_DiameterOfBinaryTree.cpp
@@ -13,6 +13,24 @@ struct TreeNode {
 };
 
 class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        findDia(root);
+        return maxDia;
+    }
+private:
+    int maxDia = 0;
+    int findDia(TreeNode* root){
+        if(root == NULL)return 0;
+        int leftDepth = findDia(root->left);
+        int rightDepth = findDia(root->right);
+        int currDia = leftDepth + rightDepth;
+        maxDia = max(maxDia, currDia);
+        return 1 + max(leftDepth, rightDepth);
+    }
+};
+
+class Solution {
     // WA
 public:
     void solve(TreeNode *root, int index, int &mini, int &maxi) {
