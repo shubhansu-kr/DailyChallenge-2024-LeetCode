cfc59c81b64c4cf0c5e7dffac9d3cbccf2d83c14
Shubhansu kumar <shubhansu2021@gmail.com>
2024-03-09 07:23:34 +0530

    Find the min. common value

---
+++ 03_March_2024/08_MinCommon.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/minimum-common-value/?envType=daily-question&envId=2024-03-09
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+#pragma GCC optimize("O3", "unroll-loops")
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int n1=nums1.size(), n2=nums2.size();
+        if (n1>n2){
+            swap(nums1, nums2);
+            swap(n1, n2);
+        }
+        int prev=0;
+        for(int i=0; i<n1; i++){
+            int x=nums1[i];
+            int j=lower_bound(nums2.begin()+prev, nums2.end(), x)-nums2.begin();
+            if (j==n2) return -1;
+            else if (x==nums2[j]) return x;
+            else// x<nums[j]
+                prev=j;
+        }
+        return -1;
+    }
+};
+
+
+int main () {
+    
+    return 0;
+}
