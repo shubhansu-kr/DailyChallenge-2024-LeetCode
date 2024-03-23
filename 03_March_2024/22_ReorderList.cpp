02d229345ab92f631456c116a1b9c92aebc029b4
Shubhansu kumar <shubhansu2021@gmail.com>
2024-03-23 23:03:59 +0530

    Reorder the list

---
+++ 03_March_2024/22_ReorderList.cpp
@@ -0,0 +1,64 @@
+// https://leetcode.com/problems/reorder-list/?envType=daily-question&envId=2024-03-23
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+struct ListNode {
+    int val;
+    ListNode * next;
+    ListNode():val(0), next(nullptr){}
+    ListNode(int val):val(val), next(nullptr){}
+    ListNode(int val, ListNode *next):val(val), next(next){}
+};
+
+
+#pragma GCC optimize("O3", "unroll-loops")
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (!head || !head->next || !head->next->next) return;
+
+        // Find the middle of the list
+        ListNode *slow = head, *fast = head;
+        while (fast->next && fast->next->next) {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+
+        // Reverse the second half of the list
+        ListNode *prev =NULL, *cur=slow->next, *Next;
+        while (cur) {
+            Next=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=Next;
+        }
+        slow->next=NULL;
+
+        // Merge the 2 halves
+        ListNode *A = head, *B=prev;
+        while (A && B) {
+            ListNode* A_next=A->next;
+            ListNode *B_next=B->next;
+
+            A->next=B;
+            B->next=A_next;
+
+            A=A_next;
+            B=B_next;
+        }
+    }
+};
+
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+int main () {
+    
+    return 0;
+}
