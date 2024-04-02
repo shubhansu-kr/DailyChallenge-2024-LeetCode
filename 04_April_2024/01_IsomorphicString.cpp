a5594b750964b0d6ab140f0d10d1529f6b9b4060
Shubhansu kumar <shubhansu2021@gmail.com>
2024-04-02 23:42:37 +0530

    Check if isometric

---
+++ 04_April_2024/01_IsomorphicString.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/isomorphic-strings/?envType=daily-question&envId=2024-04-02
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int m1[256] = {0}, m2[256] = {0}, n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (m1[s[i]] != m2[t[i]]) return false;
+            m1[s[i]] = i + 1;
+            m2[t[i]] = i + 1;
+        }
+        return true;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+int main () {
+    
+    return 0;
+}
