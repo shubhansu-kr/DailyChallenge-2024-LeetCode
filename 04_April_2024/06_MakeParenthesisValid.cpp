6ffd3e4e5c927b99d13e6a4266360388a056030d
Shubhansu kumar <shubhansu2021@gmail.com>
2024-04-07 00:10:24 +0530

    Create valid parenthesis

--- 04_April_2024/06_MakeParenthesisValid.cpp
+++ 04_April_2024/06_MakeParenthesisValid.cpp
@@ -3,7 +3,25 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
-
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> st;
+        for (auto i = 0; i < s.size(); ++i) {
+            if (s[i] == '(') st.push(i);
+            if (s[i] == ')') {
+            if (!st.empty()) st.pop();
+            else s[i] = '*';
+            }
+        }
+        while (!st.empty()) {
+            s[st.top()] = '*';
+            st.pop();
+        }
+        s.erase(remove(s.begin(), s.end(), '*'), s.end());
+        return s;
+    }
+};
 
 
 class Solution {
