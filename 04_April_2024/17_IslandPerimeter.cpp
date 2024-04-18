1813e05320b5d5e919c9025d29502cec4f7b42f5
Shubhansu kumar <shubhansu2021@gmail.com>
2024-04-18 22:31:12 +0530

    Find the perimeter of the island

---
+++ 04_April_2024/17_IslandPerimeter.cpp
@@ -0,0 +1,68 @@
+// https://leetcode.com/problems/island-perimeter/?envType=daily-question&envId=2024-04-18
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n));
+
+        queue<pair<int, int>> q;
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j) {
+                if (grid[i][j]){
+                    q.push(pair<int, int>({i, j}));
+                    visited[i][j] = true;
+                    break;
+                }
+            }
+        }
+
+        int perimeter = 0;
+
+        vector<int> shiftRow = {-1, 0, 1, 0}, shiftCol = {0, 1, 0, -1};
+
+        while(q.size()) {
+            pair<int, int> p = q.front();
+            q.pop();
+
+            int row = p.first, col = p.second();
+
+            for(int i = 0; i < 4; ++i) {
+                int newRow = row + shiftRow[i];
+                int newCol = col + shiftCol[i];
+
+                // check valid
+                if (newCol >= 0 && newRow >= 0 && newRow < m && newCol < n) {
+
+                    if (grid[newRow][newCol]) {
+                        if (!visited[newRow][newCol]){
+                            q.push(pair<int, int>({newRow, newCol}));
+                            visited[newRow][newCol] = true;
+                        }
+                    }
+                    else {
+                        ++perimeter;
+                    }
+                }
+                else {
+                    ++perimeter;
+                }
+            }
+        }
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+int main () {
+    
+    return 0;
+}
