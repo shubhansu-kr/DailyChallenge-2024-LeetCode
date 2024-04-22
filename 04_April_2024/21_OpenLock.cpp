cd79955ab0bcab4108f9e53ee32ba0169911feda
Shubhansu kumar <shubhansu2021@gmail.com>
2024-04-22 22:52:14 +0530

    Open Lock

---
+++ 04_April_2024/21_OpenLock.cpp
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/open-the-lock/?envType=daily-question&envId=2024-04-22
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int openLock(vector<string>& deadends, string target) {
+        unordered_set<string> deadSet(deadends.begin(), deadends.end());
+        if (deadSet.count("0000")) return -1;
+        queue<string> q({"0000"});
+        for (int steps = 0; !q.empty(); ++steps) {
+            for (int i = q.size(); i > 0; --i) {
+                auto curr = q.front(); q.pop();
+                if (curr == target) return steps;
+                for (auto nei : neighbors(curr)) {
+                    if (deadSet.count(nei)) continue;
+                    deadSet.insert(nei); // Marked as visited
+                    q.push(nei);
+                }
+            }
+        }
+        return -1;
+    }
+    vector<string> neighbors(const string& code) {
+        vector<string> result;
+        for (int i = 0; i < 4; i++) {
+            for (int diff = -1; diff <= 1; diff += 2) {
+                string nei = code;
+                nei[i] = (nei[i] - '0' + diff + 10) % 10 + '0';
+                result.push_back(nei);
+            }
+        }
+        return result;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+int main () {
+    
+    return 0;
+}
