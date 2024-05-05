a3290cc4a892e0e234c092882ad190f31e8f602b
Shubhansu kumar <shubhansu2021@gmail.com>
2024-05-05 11:47:50 +0530

    Delete a node

---
+++ 05_May_2024/04_DeleteNode.cpp
@@ -0,0 +1,32 @@
+// 
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+struct ListNode {
+    int val;
+    ListNode * next;
+    ListNode():val(0), next(nullptr){}
+    ListNode(int val):val(val), next(nullptr){}
+    ListNode(int val, ListNode *next):val(val), next(next){}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+int main () {
+    
+    return 0;
+}
