c3f831f59f72fac9241487cffdd2e7dae7461851
Shubhansu kumar <shubhansu2021@gmail.com>
2024-05-17 23:48:26 +0530

    Delete Leave with a given value

---
+++ 05_May_2024/16_DeleteLeave.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/delete-leaves-with-a-given-value/?envType=daily-question&envId=2024-05-17
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if (!root) return nullptr;
+        root->left = removeLeafNodes(root->left, target);
+        root->right = removeLeafNodes(root->right, target);
+        if (!root->left && !root->right && root->val == target)
+            return nullptr;
+        return root;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+int main () {
+    
+    return 0;
+}
