073478bbae6c7077200ae4522eaefb8f336a5b6e
Shubhansu kumar <shubhansu2021@gmail.com>
2024-06-06 21:23:02 +0530

    Find straight hands

---
+++ 06_June_2024/05_StraightHands.cpp
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/hand-of-straights/?envType=daily-question&envId=2024-06-06
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+   bool findSuccessors(vector<int>& hand, int groupSize, int i, int n) {
+        int next = hand[i] + 1;
+        hand[i] = -1;  // Mark as used
+        int count = 1;
+        i += 1;
+        while (i < n && count < groupSize) {
+            if (hand[i] == next) {
+                next = hand[i] + 1;
+                hand[i] = -1;
+                count++;
+            }
+            i++;
+        }
+        return count == groupSize;
+    }
+
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size();
+        if (n % groupSize != 0) return false;
+        std::sort(hand.begin(), hand.end());
+        for (int i = 0; i < n; i++) {
+            if (hand[i] >= 0) {
+                if (!findSuccessors(hand, groupSize, i, n)) return false;
+            }
+        }
+        return true;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+int main () {
+    
+    return 0;
+}
--- Test.cpp
+++ Test.cpp
@@ -1,37 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-
-#include <bits/stdc++.h>
-using namespace std ;
-
-
-
-auto init = []() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
-
-int findDup(string &s) {
-    int count = 0;
-    unordered_set<char> st;
-    for(auto &it: s) {
-        if (st.count(it)) {
-            ++count;
-        }
-        st.insert(it);
+int solve(int i, int j, int k, int m, vector<vector<vector<vector<int>>>>& dp, const vector<int>& a) {
+    if (dp[i][j][k][m] != -1) return dp[i][j][k][m];
+    if (k > m) return -(1 << 29); // impossible case
+    if (i > j) return 0;
+    if (i == j) {
+        if (a[i] >= k && a[i] <= m) return 1;
+        else return 0;
     }
-    return count;
+    int res = 0;
+    res = max(res, solve(i+1, j-1, k, m, dp, a));
+    if (a[j] >= k)
+        res = max(res, solve(i+1, j-1, a[j], m, dp, a) + 1);
+    if (a[i] <= m)
+        res = max(res, solve(i+1, j-1, k, a[i], dp, a) + 1);
+    if (k <= a[j] && a[j] <= a[i] && a[i] <= m) {
+        res = max(res, solve(i+1, j-1, a[j], a[i], dp, a) + 2);
+    }
+    res = max(res, solve(i+1, j, k, m, dp, a));
+    if (a[i] >= k)
+        res = max(res, solve(i+1, j, a[i], m, dp, a) + 1);
+    res = max(res, solve(i, j-1, k, m, dp, a));
+    if (a[j] <= m)
+        res = max(res, solve(i, j-1, k, a[j], dp, a) + 1);
+    return dp[i][j][k][m] = res;
 }
-int main () {
-    int t; 
-    cin >> t;
 
-    while(t--){
-        string s;
-        cin >> s;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-        cout << findDup(s) << endl;
-    }
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+
+    vector<vector<vector<vector<int>>>> dp(n, vector<vector<vector<int>>>(n, vector<vector<int>>(51, vector<int>(51, -1))));
+
+    cout << solve(0, n - 1, 0, 50, dp, a) << endl;
+
     return 0;
 }
