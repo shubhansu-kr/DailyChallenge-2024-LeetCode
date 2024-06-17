85a0993f182ada89bf0360479f9ac4f93c93ace3
Shubhansu kumar <shubhansu2021@gmail.com>
2024-06-17 20:16:17 +0530

    Judge Square Numbers

---
+++ 06_June_2024/16_SumOfSquareNumber.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/sum-of-square-numbers/?envType=daily-question&envId=2024-06-17
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for (int divisor = 2; divisor * divisor <= c; divisor++) {
+            if (c % divisor == 0) {
+                int exponentCount = 0;
+                while (c % divisor == 0) {
+                    exponentCount++;
+                    c /= divisor;
+                }
+                if (divisor % 4 == 3 && exponentCount % 2 != 0) {
+                    return false;
+                }
+            }
+        }
+        return c % 4 != 3;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+int main () {
+    
+    return 0;
+}
