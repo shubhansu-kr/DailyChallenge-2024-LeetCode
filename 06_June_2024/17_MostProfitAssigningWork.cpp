6745138bcd021ba12eebc1e3c910eb77bb453861
Shubhansu kumar <shubhansu2021@gmail.com>
2024-06-18 22:00:10 +0530

    Find the most Profit assignment

---
+++ 06_June_2024/17_MostProfitAssigningWork.cpp
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/most-profit-assigning-work/?envType=daily-question&envId=2024-06-18
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+#define sort(a) sort(begin(a), end(a))
+#define opt() ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        opt();
+        int res = 0, j = 0, best = 0;
+        vector<pair<int, int>> temp;
+        for(int i = 0; i < worker.size(); ++i) {
+            temp.push_back({difficulty[i], profit[i]});
+        }
+
+        sort(temp); sort(worker);
+        for(int work : worker) {
+            while(work >= temp[j].first && j < worker.size()) {
+                best = max(best, temp[j++].second);
+            }
+            
+            res += best;
+        }
+
+        return res;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+int main () {
+    
+    return 0;
+}
