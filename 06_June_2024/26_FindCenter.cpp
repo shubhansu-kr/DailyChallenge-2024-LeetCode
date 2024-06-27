1ef3fd438b31609f7819c904e3689647ce77fa76
Shubhansu kumar <shubhansu2021@gmail.com>
2024-06-27 17:45:39 +0530

    Find the center of the Star graph

---
+++ 06_June_2024/26_FindCenter.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/find-center-of-star-graph/?envType=daily-question&envId=2024-06-27
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+int main () {
+    
+    return 0;
+}
