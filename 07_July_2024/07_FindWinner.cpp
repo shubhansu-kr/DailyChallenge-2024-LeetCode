08c2de0dc1d417e78ea9e4c3bbe70bb238f2e771
Shubhansu kumar <shubhansu2021@gmail.com>
2024-07-08 20:16:14 +0530

    Find the winner

---
+++ 07_July_2024/07_FindWinner.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/find-the-winner-of-the-circular-game/?envType=daily-question&envId=2024-07-08
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+    // BruteForce : Simulate
+    // Not getting anywhere 
+public:
+    int findTheWinner(int n, int k) {
+        vector<int> winner(n, 1);
+        int p = 0, q = n;
+        while(q != 1) {
+            p = (p + k) % n;
+            winner[p-1] = 0;
+            q--;
+        }
+
+        for(int i = 0; i < winner.size(); ++i) {
+            if (winner[i]) return i+1;
+        }
+
+        return q;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+int main () {
+    
+    return 0;
+}
