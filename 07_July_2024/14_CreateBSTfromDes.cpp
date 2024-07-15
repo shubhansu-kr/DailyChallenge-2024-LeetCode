9e0668f967a32f361ca7ecaaab9490c64a2885b8
Shubhansu kumar <shubhansu2021@gmail.com>
2024-07-15 20:02:20 +0530

    WA

--- 07_July_2024/14_CreateBSTfromDes.cpp
+++ 07_July_2024/14_CreateBSTfromDes.cpp
@@ -14,7 +14,60 @@ struct TreeNode {
 
 class Solution {
     // BruteForce
+    // Wrong answer : [[85,82,1],[74,85,1],[39,70,0],[82,38,1],[74,39,0],[39,13,1]]
+    // Reason : Ct.insert Is in wrong position
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_set<int> st, ct;
+        unordered_map<int, TreeNode *> mp;
+        
+        TreeNode *head = nullptr;
+
+        for(auto &it: descriptions) {
+            int parent = it[0], child = it[1], isLeft = it[2];
+
+            TreeNode *q = nullptr;
+            if (st.count(child)) {
+                q = mp[child];
+            }
+            else {
+                q = new TreeNode(child);
+                st.insert(child);
+                mp[child] = q;
+                ct.insert(child);
+            }
+
+
+            TreeNode *p = nullptr;
+            if (st.count(parent)) {
+                p = mp[parent];
+            }
+            else {
+                p = new TreeNode(parent);
+                st.insert(parent);
+                mp[parent] = p;
+            }
+
+            if (isLeft) {
+                p->left = q;
+            }
+            else {
+                p->right = q;
+            }            
+        }
+
+        for(auto &it: mp) {
+            if (!ct.count(it.first)) return it.second;
+        }
+        return head;
+    }
+};
+
+
+class Solution {
+    // BruteForce
     // Wrong Answer : [[85,82,1],[74,85,1],[39,70,0],[82,38,1],[74,39,0],[39,13,1]]
+    // Reason : Head is not placed properly. 
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
         unordered_set<int> st;
