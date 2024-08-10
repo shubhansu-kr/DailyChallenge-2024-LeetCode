1a1f6aa328cda92100f5b579b55ca5bae9eee96a
Shubhansu kumar <shubhansu2021@gmail.com>
2024-08-10 23:21:04 +0530

    Test OA - AMAZON

--- Test.cpp
+++ Test.cpp
@@ -1,30 +1,42 @@
-// 
+// Mock Test - Amazon
 
 #include <bits/stdc++.h>
 using namespace std ;
 
-int getMinCost(int N, vector<int> A) {
-    vector<int> dp(N, INT_MAX);
-    dp[0] = 0;
+int findMaxZeroes(vector<int> arr) {
+    int n = arr.size();
+    vector<int> leftMin(n);
 
-    for (int i = 0; i < N; ++i) {
-        if (i + 1 < N) {
-            dp[i + 1] = min(dp[i + 1], dp[i] + 1);
-        }
-
-        for (int j = i + 1; j < N; ++j) {
-            if (A[j] % A[i] == 0) {
-                if (dp[i]+ A[i] + 1 < dp[j]){
-                    dp[j] = dp[i] + A[i] + 1;
-                }
-                A[j]++; 
-            }
-        }
+    int mini = INT_MAX;
+    for(int i = 0; i < n; ++i) {
+        mini = min(arr[i], mini);
+        leftMin[i] = mini;
     }
 
-    return dp[N-1];
+    int count = 0; 
+    for(int i = 0; i < n; ++i) {
+        count += (arr[i] == leftMin[i]);
+    }
+    return count;
 }
+int getOutlierValue(vector<int> arr) {
 
+    map<int, int> mp;
+    int sum = 0;
+    int sol = 0;
+
+    for(int i=0; i<arr.size(); i++){
+        sum += arr[i];
+        mp[arr[i]]++;
+    }
+
+    for(int i=0; i<arr.size(); i++)
+    {
+        if(mp.find((sum-arr[i])/2)!=mp.end() && arr[i]!=(sum-arr[i])/2 && (sum-arr[i])%2==0) sol = max(sol, arr[i]);   
+    }
+    
+    return sol;
+}
 
 auto init = []() {
     ios::sync_with_stdio(0);
@@ -34,9 +46,8 @@ auto init = []() {
 }();
 
 int main () {
-    int N = 5; 
-    vector<int> A = {2, 4, 7, 11, 13, 17, 19, 23, 27, 25};
-
-    cout << getMinCost(N, A);
+    
+    vector<int> arr = {4,1,2,1, 10, 3};
+    cout << getOutlierValue(arr);
     return 0;
 }
