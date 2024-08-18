702217a5826528df0b7f87e6a7fcc7fd10d6cf9a
Shubhansu kumar <shubhansu2021@gmail.com>
2024-08-18 20:14:42 +0530

    WA

--- 08_August_2024/17_UglyNumberII.cpp
+++ 08_August_2024/17_UglyNumberII.cpp
@@ -4,10 +4,48 @@
 using namespace std ;
 
 class Solution {
+    // WA : Does not increment in succession
+public:
+    int nthUglyNumber(int n) {
+        vector<int> ugly(1, 1);
+        unordered_set<int> dis;
+        
+        int index = 0; 
+        while(ugly.size() < n) {
+            int sub = ugly[index];
+            int tw = sub * 2, th = sub * 3, f = sub * 5;
+            if (!dis.count(tw)) {
+                ugly.emplace_back(tw);
+                dis.insert(tw);
+                if (ugly.size() == n) break;
+            }
+            if (!dis.count(th)) {
+                ugly.emplace_back(th);
+                dis.insert(th);
+                if (ugly.size() == n) break;
+            }
+            if (!dis.count(f)) {
+                ugly.emplace_back(f);
+                dis.insert(f);
+                if (ugly.size() == n) break;
+            }
+            ++index;
+        }
+
+        int ans = INT_MIN;
+        for(auto &it: ugly) {
+            cout << it << " ";
+            if (it > ans) ans = it;
+        }
+        return ans;
+    }
+};
+
+class Solution {
     // bruteForce : TLE 
 public:
     void solve(int k, vector<bool> &dp) {
-        if (k >= sz || dp[k]) return;
+        if (k >= dp.size() || dp[k]) return;
 
         dp[k] = true;
         solve(k*2, dp);
@@ -53,7 +91,7 @@ auto init = []() {
 }();
 
 int main () {
-    Solution obj;
-    cout << obj.nthUglyNumber(1);
+    Solution1 obj;
+    cout << obj.nthUglyNumber(10);
     return 0;
 }
