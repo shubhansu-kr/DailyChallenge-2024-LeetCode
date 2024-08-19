08eaaad054f17e5c03392b15a037a8ab79675d43
Shubhansu kumar <shubhansu2021@gmail.com>
2024-08-19 22:03:04 +0530

    Solution1: Memoization

--- 08_August_2024/18_TwoKeyBoards.cpp
+++ 08_August_2024/18_TwoKeyBoards.cpp
@@ -3,6 +3,33 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution1 {
+    // Recursion: Memoisation 
+public:
+    int solve(int &n, int cp, int len, vector<vector<int>> &dp) {
+        if (len > n) return 10e6;
+        if (len == n) return 0;
+
+        if(dp[cp][len] != -1) return dp[cp][len];
+
+        int copyCurrent = INT_MAX;
+        if (cp != len) {
+            copyCurrent = 1 + solve(n, len, len, dp);
+        }
+        int pasteCurrent = INT_MAX;
+        if (cp) {
+            pasteCurrent = 1 + solve(n, cp, len+cp, dp);
+        }
+
+        return dp[cp][len] = min(copyCurrent, pasteCurrent);
+    }
+
+    int minSteps(int n) {
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        return solve(n, 0, 1, dp);
+    }
+};
+
 class Solution {
     // BruteForce : Recursion
 public:
