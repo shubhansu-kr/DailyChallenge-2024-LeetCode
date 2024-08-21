5819f7a8973072447dce7c554f086066d362f114
Shubhansu kumar <shubhansu2021@gmail.com>
2024-08-21 22:45:12 +0530

    Find the strange printer turns

---
+++ 08_August_2024/20_StrangePrinter.cpp
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/strange-printer/?envType=daily-question&envId=2024-08-21
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution
+{
+public:
+    int n;
+    vector<vector<int>> dp;
+    
+    int f(int i, int j, string &s)
+    {
+        if (dp[i][j] != -1) return dp[i][j];
+        if (i == j) return dp[i][j] = 1;
+
+        int ans;
+
+        if (s[i] == s[j] || s[j - 1] == s[j]) ans = f(i, j - 1, s);
+        else if (s[i] == s[i + 1]) ans = f(i + 1, j, s);
+        else
+        {
+            ans = f(i, j - 1, s) + 1;
+            for (int k = i + 1; k < j; k++) if (s[k] == s[j]) ans = min(ans, f(i, k - 1, s) + f(k, j - 1, s));
+        }
+        return dp[i][j] = ans;
+    }
+
+    int strangePrinter(string s)
+    {
+        n = s.size();
+        dp.assign(n, vector<int>(n, -1));
+        return f(0, n - 1, s);
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+int main () {
+    
+    return 0;
+}
